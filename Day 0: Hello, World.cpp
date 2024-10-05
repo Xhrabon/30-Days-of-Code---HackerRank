cc8ec2529b167eec8e0a8decbe2cafc1ca7543f1
Shrabon Somadder <130784151+Xhrabon@users.noreply.github.com>
2024-10-05 15:04:52 +0600

    Day 0: Hello, World.cpp

---
+++ Day 0: Hello, World.cpp
@@ -0,0 +1,15 @@
+/* Input : Welcome to 30 Days of Code!
+   Output : Hello, World. 
+            Welcome to 30 Days of Code! */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  string A = "Hello, World." , input;
+  getline(cin, input);
+
+  cout<<A<<"\n";
+  cout<<input;
+}
